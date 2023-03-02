833fc46d1584f75a04eab26237e0a8cc432557dc
AHMET BORAN OZUM <e2380814@ceng.metu.edu.tr>
2023-03-02 20:43:26 +0300

    Introduced JWT

---
+++ src/main/java/com/projects/expensetracker/security/JwtTokenProvider.java
@@ -0,0 +1,52 @@
+package com.projects.expensetracker.security;
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${jwt.secret}")
+    private String APP_SECRET;
+
+    @Value("${jwt.expiration}")
+    private Long EXPIRATION_TIME;
+
+    public String generateToken(Authentication auth) {
+        JwtUserDetails user = (JwtUserDetails) auth.getPrincipal();
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + EXPIRATION_TIME);
+
+        return Jwts.builder().setSubject(Long.toString(user.getId())).setIssuedAt(new Date()).setExpiration(expiryDate)
+                .signWith(io.jsonwebtoken.SignatureAlgorithm.HS512, APP_SECRET).compact();
+    }
+
+    Long getUserIdFromJWT(String token) {
+        return Long.parseLong(Jwts.parser().setSigningKey(APP_SECRET).parseClaimsJws(token).getBody().getSubject());
+    }
+
+    Boolean validateToken(String authToken) {
+        try {
+            Jwts.parser().setSigningKey(APP_SECRET).parseClaimsJws(authToken);
+            return !isTokenExpired(authToken);
+        } catch (io.jsonwebtoken.MalformedJwtException e) {
+            System.out.println("Invalid JWT signature.");
+        } catch (io.jsonwebtoken.ExpiredJwtException e) {
+            System.out.println("Expired JWT token.");
+        } catch (io.jsonwebtoken.UnsupportedJwtException e) {
+            System.out.println("Unsupported JWT token.");
+        } catch (IllegalArgumentException e) {
+            System.out.println("JWT claims string is empty.");
+        }
+        return false;
+    }
+
+    Boolean isTokenExpired(String token) {
+        Date expiration = Jwts.parser().setSigningKey(APP_SECRET).parseClaimsJws(token).getBody().getExpiration();
+        return expiration.before(new Date());
+    }
+}
---
+++ src/main/java/com/projects/expensetracker/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,33 @@
+package com.projects.expensetracker.service.impl;
+
+import com.projects.expensetracker.model.User;
+import com.projects.expensetracker.repository.UserRepository;
+import com.projects.expensetracker.security.JwtUserDetails;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+        return JwtUserDetails.create(user);
+    }
+
+    public UserDetails loadUserById(Long id) {
+        User user = userRepository.findById(id).orElseThrow(
+                () -> new UsernameNotFoundException("User not found with id : " + id)
+        );
+        return JwtUserDetails.create(user);
+    }
+}
